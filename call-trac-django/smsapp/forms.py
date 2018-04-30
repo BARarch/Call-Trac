029f1df615c0929ffd9b21684f442f25fecb0946
Anthony Quivers <anthonyquivers@gmail.com>
2018-04-30 00:52:08 -0400

    Imput field updates

--- call-trac-django/smsapp/forms.py
+++ call-trac-django/smsapp/forms.py
@@ -1,6 +1,21 @@
 from django import forms
 
 class SMSForm(forms.Form):
-	name = forms.CharField(max_length=120)
-	number = forms.CharField(max_length=20)
-	message = forms.CharField(max_length=160)
+	name = forms.CharField(max_length=120,
+		widget=forms.TextInput(attrs={'class': "form-control col-12",
+									  'type':"text",
+									  'placeholder':"Name ",
+									   'aria-label':"Name",
+									   'aria-describedby':"basic-addon2"}))
+	number = forms.CharField(max_length=20,
+		widget=forms.TextInput(attrs={'class': "form-control",
+									  'type':"text",
+									  'placeholder':"Number ex:+18885552222",
+									   'aria-label':"Number",
+									   'aria-describedby':"basic-addon2"}),)
+	message = forms.CharField(max_length=160,
+		widget=forms.TextInput(attrs={'class': "form-control",
+									  'type':"text",
+									  'placeholder':"Enter Message...",
+									   'aria-label':"Enter Message...",
+									   'aria-describedby':"basic-addon2"}),)
