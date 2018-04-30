0d48c979e44219737dedd98aefb58c2d2dc12104
Anthony Quivers <anthonyquivers@gmail.com>
2018-04-30 01:37:39 -0400

    Input field Boostrap Elements

--- call-trac-django/smsapp/forms.py
+++ call-trac-django/smsapp/forms.py
@@ -2,19 +2,19 @@ from django import forms
 
 class SMSForm(forms.Form):
 	name = forms.CharField(max_length=120,
-		widget=forms.TextInput(attrs={'class': "form-control col-12",
+		widget=forms.TextInput(attrs={'class': "form-control input-sm",
 									  'type':"text",
 									  'placeholder':"Name ",
 									   'aria-label':"Name",
 									   'aria-describedby':"basic-addon2"}))
 	number = forms.CharField(max_length=20,
-		widget=forms.TextInput(attrs={'class': "form-control",
+		widget=forms.TextInput(attrs={'class': "form-control input-sm",
 									  'type':"text",
 									  'placeholder':"Number ex:+18885552222",
 									   'aria-label':"Number",
 									   'aria-describedby':"basic-addon2"}),)
 	message = forms.CharField(max_length=160,
-		widget=forms.TextInput(attrs={'class': "form-control",
+		widget=forms.TextInput(attrs={'class': "form-control input-lg",
 									  'type':"text",
 									  'placeholder':"Enter Message...",
 									   'aria-label':"Enter Message...",
